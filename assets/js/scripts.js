cbdcc7d69c6731f79542b26da4cb9dd1c34beb91
zakaryafactory <44473422+zakaryafactory@users.noreply.github.com>
2024-12-11 11:43:29 +0100

    Ajout javascript

---
+++ assets/js/scripts.js
@@ -0,0 +1,59 @@
+// DOM selection
+const lightbox = document.querySelector('.lightbox');
+const lightboxImage = document.querySelector('.lightbox__image');
+const images = document.querySelectorAll('.gallery__item img');
+const lightboxCloseButton = document.querySelector('.lightbox__closeButton');
+const lightboxBackground = document.querySelector('.lightbox__background');
+
+const reservationForm = document.querySelector('.reservation__form');
+const nameInput = document.querySelector('.form__input--name');
+
+//////////////////////////////////////////////////////////////
+// LIGHTBOX
+/////////////////////////////////////////////////////////////
+
+// Ouverture de la lightbox
+images.forEach((image) => {
+  image.addEventListener('click', () => {
+    lightboxImage.innerHTML = '<img src="' + image.src + '" />';
+    lightbox.style.visibility = 'visible';
+  });
+});
+
+// Fermeture de la lightbox
+lightboxCloseButton.addEventListener('click', () => {
+  lightboxImage.innerHTML = '';
+  lightbox.style.visibility = 'hidden';
+});
+lightboxBackground.addEventListener('click', () => {
+  lightboxImage.innerHTML = '';
+  lightbox.style.visibility = 'hidden';
+});
+
+//////////////////////////////////////////////////////////////
+// Formulaire
+/////////////////////////////////////////////////////////////
+
+// Contrôle des champs
+reservationForm.addEventListener('submit', (e) => {
+  let submitting = true;
+  e.preventDefault();
+  // champ nom (minimun 3 caractètres)
+  if (nameInput.value.length <= 3) {
+    submitting = false;
+    alert('Veuillez saisir au minimum 3 caractères dans le champ nom');
+  }
+
+  if (submitting) {
+    alert('Formulaire valide');
+    //reservationForm.submit();
+  }
+});
+
+nameInput.addEventListener('keypress', () => {
+  if (nameInput.value.length + 1 <= 3) {
+    nameInput.style.backgroundColor = '#f8d4d4  ';
+  } else {
+    nameInput.style.backgroundColor = 'white';
+  }
+});
